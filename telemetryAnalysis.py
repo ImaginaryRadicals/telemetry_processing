df6aed7ea6663df23a07eb3bccd520453a0ca071
Ashley Trowell <ajtrowell@gmail.com>
2017-12-21 23:37:59 -0600

    Added plotMotorData(data, motorName) to plot power and ticks together.

--- telemetryAnalysis.py
+++ telemetryAnalysis.py
@@ -6,7 +6,7 @@ Created on Thu Dec 21 21:00:52 2017
 """
 
 # getCSVTable
-import parseCSV as csv
+import parseCSV
 
 #import csv
 import matplotlib.pyplot as plt
@@ -43,10 +43,9 @@ def diff(value,time,maxRate=-1):
 def plotJewelArm(dataTable):
     time = dataTable['time']
     jewelArm = dataTable['JEWEL_ARM']
-    plt.figure(1)
     plt.clf()
     plt.title("Jewel Arm over time")
-    plt.plot(time,jewelArm, 'r--', label='jewelArm')
+    plt.plot(time,jewelArm, '-', label='jewelArm')
     plt.xlabel('Time (s)')
     plt.ylabel('Jewel Arm Servo [0,1]')
     plt.legend()
@@ -57,7 +56,6 @@ def plotXYPosition(dataTable):
     x = dataTable['x_in']
     y = dataTable['y_in']
     theta = dataTable['theta_rad']
-    plt.figure(2)
     plt.clf()
     plt.title("Mecanum Navigation Position")
     plt.plot(x,y, 'r--.', label='position')
@@ -71,7 +69,6 @@ def plotMotorTickRateVsPower(dataTable):
     motorTicks = dataTable['DRIVE_FRONT_LEFT_ticks']
     motorTickRate = diff(motorTicks,time)
     motorPower = dataTable['DRIVE_FRONT_LEFT_power']
-    plt.figure(3)
     plt.clf()
     plt.title("Motor Ticks vs Power")
     plt.plot(motorPower[0:-1], motorTickRate, 'b.', label='position')
@@ -79,14 +76,50 @@ def plotMotorTickRateVsPower(dataTable):
     plt.ylabel('Tick Rate')
     plt.legend()
     plt.show()
+    
+def plotMotorData(dataTable, motorName):
+    time = dataTable['time']
+    motorPower = dataTable[motorName + '_power']
+    motorTicks = dataTable[motorName + '_ticks']
+    
+    fig, ax1 = plt.subplots()    
+    plt.title(motorName + " MOTOR")
+    ax1.plot(time, motorPower, 'r.', label='power')
+    ax1.set_xlabel('time (s)')
+    # Make the y-axis label, ticks and tick labels match the line color.
+    ax1.set_ylabel('Motor Power', color='r')
+    ax1.tick_params('y', colors='r')
+    
+    # Create twin axis
+    ax2 = ax1.twinx()
+    ax2.plot(time, motorTicks, 'b.', label='ticks')
+
+    ax2.set_ylabel('Encoder Ticks', color='b')
+    ax2.tick_params('y', colors='b')
+    
+    fig.tight_layout()
+#    plt.legend()
+    plt.show()
+    
 
 #%% Main routine, run if module is executed directly, rather than imported.
 
 def main():
-    dataTable = csv.getDictionary()
+    dataTable = parseCSV.getDictionary('telemetry1_defectiveBackLeftEncoder.csv')
+    plt.close("all")
+    
+    plt.figure()
     plotJewelArm(dataTable)
+    plt.figure()
     plotXYPosition(dataTable)
-    plotMotorTickRateVsPower(dataTable)
+#    plt.figure()
+#    plotMotorTickRateVsPower(dataTable)
+    
+    # Display data for all motors
+    driveMotors = ['DRIVE_FRONT_LEFT','DRIVE_FRONT_RIGHT',
+                   'DRIVE_BACK_LEFT','DRIVE_BACK_RIGHT']
+    for i,motor in enumerate(driveMotors):
+        plotMotorData(dataTable,motor)
 
 
 if __name__ == '__main__':
