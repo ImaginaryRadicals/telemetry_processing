9a44af31ed19b780af335a46e5cad8de34ed8fd5
Ashley Trowell <ajtrowell@gmail.com>
2017-12-21 18:36:44 -0600

    getCSVTable() takes an optional filename, returns a dataTable
    If the module is run directly, a jewel arm plot
    is displayed.

--- parseCSV.py
+++ parseCSV.py
@@ -6,11 +6,65 @@ Created on Wed Dec 20 03:15:32 2017
 """
 
 import csv
+import matplotlib.pyplot as plt
+# File Dialog
+import tkinter as tk
+from tkinter import filedialog
 
-with open('telemetry1.csv', 'r') as csvfile:
-    reader = csv.reader(csvfile, delimiter=',', quotechar='"')
-    fieldNames = next(reader)
-    print(fieldNames)
-    for row in reader:
-        print(', '.join(row))
-        
+
+def getCSVTable(filename=0):
+    if filename==0:
+        # Open file dialog if no filename is given.
+        root = tk.Tk()
+        root.withdraw()
+        filename = filedialog.askopenfilename()
+
+    dataTable = []
+    fieldNames = []
+
+    with open('telemetry1.csv', 'r') as csvfile:
+        reader = csv.reader(csvfile, delimiter=',', quotechar='"')
+        # Clean up field names
+        fieldNames = next(reader)
+        for i, field in enumerate(fieldNames):
+            fieldNames[i] = fieldNames[i].strip()
+        print(fieldNames)
+
+        for row in reader:
+            thisRecord = {}
+            for i, item in enumerate(row):
+                try:
+                    thisRecord[fieldNames[i]] = float(item)
+                except:
+                    thisRecord[fieldNames[i]] = item
+
+            dataTable.append(thisRecord)
+    return dataTable
+
+
+#%%
+def plotJewelArm(dataTable):
+    # Extract data as lists using list comprehensions.
+    time = [record['time'] for record in dataTable]
+    jewelArm = [record['JEWEL_ARM'] for record in dataTable]
+
+    plt.figure(1)
+    plt.clf()
+    plt.title("Jewel Arm over time")
+    plt.plot(time,jewelArm, 'r--', label='jewelArm')
+    plt.xlabel('Time (s)')
+    plt.ylabel('Jewel Arm Servo [0,1]')
+    plt.legend()
+    plt.show()
+
+
+def main():
+
+    dataTable = getCSVTable('telemetry1.csv')
+    plotJewelArm(dataTable)
+
+
+if __name__ == '__main__':
+    main()
+
+
