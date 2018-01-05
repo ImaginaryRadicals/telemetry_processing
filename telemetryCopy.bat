18f87404aac3aff5d650c39c3688d2d66dc01357
Ashley Trowell <ajtrowell@gmail.com>
2018-01-04 19:57:30 -0600

    Updated batch file for constants.csv and controls.csv
    Updated batch file so it pulls all 3 csv files:
    telemetry.csv
    constants.csv
    controls.csv

--- telemetryCopy.bat
+++ telemetryCopy.bat
@@ -4,14 +4,30 @@ ECHO View connected devices
 ECHO.
 adb devices -l
 ECHO.
-ECHO Copy telemetry.csv to /sdcard/ from 
-ECHO /data/data/com.qualcomm.ftcrobotcontroller/files/
+ECHO From /data/data/com.qualcomm.ftcrobotcontroller/files/
+ECHO copy telemetry.csv to /sdcard/
 ECHO.
 adb shell "run-as com.qualcomm.ftcrobotcontroller cp /data/data/com.qualcomm.ftcrobotcontroller/files/telemetry.csv /sdcard/.;"
 ECHO.
+ECHO copy constants.csv to /sdcard/
+ECHO.
+adb shell "run-as com.qualcomm.ftcrobotcontroller cp /data/data/com.qualcomm.ftcrobotcontroller/files/constants.csv /sdcard/.;"
+ECHO.
+ECHO copy controls.csv to /sdcard/
+ECHO.
+adb shell "run-as com.qualcomm.ftcrobotcontroller cp /data/data/com.qualcomm.ftcrobotcontroller/files/controls.csv /sdcard/.;"
+ECHO.
+
 ECHO Pull telemetry.csv from /sdcard/ to local folder
 CHDIR
 adb pull /sdcard/telemetry.csv .
+ECHO Pull constants.csv from /sdcard/ to local folder
+CHDIR
+adb pull /sdcard/constants.csv .
+ECHO Pull controls.csv from /sdcard/ to local folder
+CHDIR
+adb pull /sdcard/controls.csv .
+
 ECHO.
 ECHO Script Complete
 ECHO.
